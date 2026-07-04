c17798fb8d4a8e36b65313450fa3a03c8a88087a
Ashmi <ashmi.sharma02@gmail.com>
2026-07-04 10:05:52 +0800

    Initial Commit

---
+++ ScreenApp (Ashmi)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenApp (Ashmi)
+//
+//  Created by Ashmi Sharma on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenApp (Ashmi)/ScreenApp__Ashmi_App.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenApp__Ashmi_App.swift
+//  ScreenApp (Ashmi)
+//
+//  Created by Ashmi Sharma on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenApp__Ashmi_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

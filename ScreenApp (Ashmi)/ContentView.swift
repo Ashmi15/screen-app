be0df77567ad05be50f6ec9615b0b61e9519febc
Ashmi <ashmi.sharma02@gmail.com>
2026-07-04 10:15:57 +0800

    Added background color and name.

--- ScreenApp (Ashmi)/ContentView.swift
+++ ScreenApp (Ashmi)/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.green
+            Text("Ashmi Sharma")
+                .foregroundStyle(.white)
+                .bold()
+                .font(.largeTitle)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
